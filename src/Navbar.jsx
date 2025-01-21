eff786d60aa4b4c6256e1293b7f55081813f4a3c
pooYAYooq <pooyayooq@gmail.com>
2025-01-22 01:10:47 +1100

    feat: enhance Navbar component with animated links and improved styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,26 +1,42 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { FaBars } from 'react-icons/fa6';
 import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
   const toggleLinks = () => {
     setShowLinks(!showLinks);
   };
 
+  const linkStyle = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : '0px',
+  };
+
+  const navStyle = {
+    paddingBottom: showLinks ? '2rem' : '0px',
+  }
+
   return (
-    <div className="nav-center">
-      <div className="nav-header">
-        <img src={logo} className="logo" alt="logo" />
-        <button className="nav-toggle" onClick={toggleLinks}>
-          <FaBars />
-        </button>
-      </div>
-      {showLinks && (
-        <div className="links-container">
-          <ul className="links">
+    <nav style={navStyle}>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyle}
+        >
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
@@ -31,8 +47,8 @@ const Navbar = () => {
             })}
           </ul>
         </div>
-      )}
-    </div>
+      </div>
+    </nav>
   );
 };
 export default Navbar;
